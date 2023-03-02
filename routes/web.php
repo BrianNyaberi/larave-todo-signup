c896a0d702785f55faeef87925a61d55cce79c43
Tech Savvy <briannyaberi1@.gmail.com>
2023-03-02 10:38:09 +0300

    TodosController, resource routes

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Resource route - returns all functions
+Route::resource('todos', 'App\Http\Controllers\TodosController');
